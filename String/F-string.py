a5f690c41570823eed621092f7a7b47c5f8210b7
varnit <varnitsingh763181@gmail.com>
2025-05-08 16:17:28 +0530

    f-sring change

--- String/F-string.py
+++ String/F-string.py
@@ -1,16 +1,17 @@
 # important 
 
-letter = "Hey my name is {1} and I am from {0}"
+letter = "Hey my name is {} and I am from {}"
 country = "India"
 name = "Harry"
 
+print(letter.format(country, name))  #  {1} or {0} kar do to sahi print hoga // string formating me ye ek diadvantage hai 
+
 # after using f-string 
 
-print(letter.format(country, name))
 print(f"Hey my name is {name} and I am from {country}")
 print(f"We use f-strings like this: Hey my name is {{name}} and I am from {{country}}")   # do bar {{}} ye lagane pe same chig output me aayega 
 price = 49.09999
-txt = f"For only {price:.2f} dollars!"
+txt = f"For only {price:.2f} dollars!"  # 2f ka use - desimal ke bad two digit lane ke liye
 print(txt)
 # print(txt.format())
 print(type(f"{2 * 30}"))
